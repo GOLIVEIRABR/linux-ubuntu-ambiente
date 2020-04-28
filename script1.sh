816f0ba3036690d152c9cd1b309c444f0c56a2dc
Geazi Oliveira <geazioliveira@hotmail.com>
2020-04-27 22:25:19 -0300

    correçao de configuraçao do vs code

--- script1.sh
+++ script1.sh
@@ -59,7 +59,7 @@ git config --global user.name "Geazi Oliveira" && git config --global user.email
 echo "==========CONFIGURANDO VSCODE=========="
 ###########COnfigurações de Desenvolvimento########
 ##Configurando o VSCode
-mkdir -p ~/.config/teste/User/
+mkdir -p ~/.config/Code/User/
 cd ~/.config/Code/User/
 echo '{
 
